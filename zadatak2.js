3a142b465d341124de4b92e288506d945e9dbc09
FilipSlavic5 <slavic@unipu.hr>
2022-10-11 08:39:18 +0200

    Rijesen 2 zadatak

---
+++ zadatak2.js
@@ -0,0 +1,8 @@
+function first_half (str) {
+    if (str.length % 2 == 0) {
+      return str.slice(0, str.length / 2);
+    }
+    return str;
+  }
+  console.log(first_half("Python"));  
+  
--- zadatak3.js
+++ zadatak3.js
@@ -4,4 +4,4 @@ const nums = arr.filter(n => typeof n == "number").sort((a, b) => a - b);
 const non_nums = arr.filter(x => typeof x != "number").sort(); 
 
 const res = [...nums, ...non_nums]; // combine the two arrays
-console.log(res); // [-1, 5, 9, "2", "3", "ab"]
+console.log(res); // [-1, 5, 9, "2", "3", "gas"]
