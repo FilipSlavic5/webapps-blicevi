0af305e32748fb799e9cf892f1896d7aaa4680f4
FilipSlavic5 <slavic@unipu.hr>
2022-10-11 08:40:54 +0200

    Rjesen zadatak 3

--- zadatak3.js
+++ zadatak3.js
@@ -3,5 +3,5 @@ const arr = [9, 5, '2', 'ab', '3', -1];
 const nums = arr.filter(n => typeof n == "number").sort((a, b) => a - b); 
 const non_nums = arr.filter(x => typeof x != "number").sort(); 
 
-const res = [...nums, ...non_nums]; // combine the two arrays
-console.log(res); // [-1, 5, 9, "2", "3", "gas"]
+const res = [...nums, ...non_nums]; 
+console.log(res); 
