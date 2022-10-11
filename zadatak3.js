aad23269a194f39338b0d489e303224bc7d45766
FilipSlavic5 <slavic@unipu.hr>
2022-10-11 08:31:16 +0200

    Rijesen zadatak 3

---
+++ zadatak3.js
@@ -0,0 +1,13 @@
+let scores = [
+    9, 80, 10, 20, 5, 70
+];
+scores.sort((a, b) => a - b);
+
+let animals = [
+    'cat', 'dog', 'elephant', 'bee', 'ant'
+];
+animals.sort();
+
+console.log(animals);
+
+console.log(scores);
